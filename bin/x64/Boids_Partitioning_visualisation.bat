0998d4e9d69cce6be85defe5e3c78b8cd23e1fea
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2016-08-05 11:09:26 +0100

    Removed logging from boids example batch file.

--- bin/x64/Boids_Partitioning_visualisation.bat
+++ bin/x64/Boids_Partitioning_visualisation.bat
@@ -1,1 +1,1 @@
-"Release_Visualisation\Boids_Partitioning.exe" "..\..\examples\Boids_Partitioning\iterations\0.xml" > log.txt
+"Release_Visualisation\Boids_Partitioning.exe" "..\..\examples\Boids_Partitioning\iterations\0.xml"
