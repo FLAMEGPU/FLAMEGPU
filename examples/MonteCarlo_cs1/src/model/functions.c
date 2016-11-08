a795cb4d5b4c6d41e627e7eb66747e56f446bd01
Mozhgan K. Chimeh <mozhgan.kabiri@gmail.com>
2016-11-08 16:47:26 +0000

    added Hist

--- examples/MonteCarlo_cs1/src/model/functions.c
+++ examples/MonteCarlo_cs1/src/model/functions.c
@@ -21,7 +21,7 @@
 
 #include <header.h>
 
-#define I_agg 0.0.875f // high index aggregation
+#define I_agg 0.875f // high index aggregation
 #define aggNo 1
 
 float *d_dt;
@@ -41,31 +41,38 @@ __FLAME_GPU_INIT_FUNC__ void initConstants()
 	printf("FLAME GPU Init function. aggno=%f, I_agg=%d\n", aggno, I_agg);
 }
 
+
 // The function calculates the time step needed for CASE STUDY 1 - NOT USING IT !
 __FLAME_GPU_STEP_FUNC__ void DELTA_T_func(){
 
-    beta0 = h_I_agg; // it's the aggregation number
-    
-    gpuErrchk(cudaMemcpyFromSymbol( &popnNo,d_xmachine_memory_crystal_count, sizeof(float))); // Nc-aggNo
+	beta0 = h_I_agg; // it's the aggregation number
+
+	gpuErrchk(cudaMemcpyFromSymbol( &popnNo,d_xmachine_memory_crystal_count, sizeof(float))); // Nc-aggNo
 
 	float dt = ((2*h_aggNo)/(beta0*popnNo*(1-h_I_agg)*(popnNo-h_aggNo))); // equation 10 and 11 
 
-   	cudamalloc((void**)&d_dt, sizeof(float));
-    gpuErrchk(cudaMemcpy(d_dt, &dt, sizeof(float), cudaMemcpyHostToDevice));
+	cudamalloc((void**)&d_dt, sizeof(float));
+	gpuErrchk(cudaMemcpy(d_dt, &dt, sizeof(float), cudaMemcpyHostToDevice));
 
 
 	printf("FLAME GPU Step function. Delta T is %f\n", dt);
 }
 
+
 __FLAME_GPU_EXIT_FUNC__ void exitFunction(){
 
 	printf("FLAME GPU Exit function");
+	for (int i=0; i<BIN_COUNT; i++){
+		int count = count_crystal_default_bin_variable(i);
+		printf("bin index=%d, count = %d", i, count);
+		//output into same format as initial states
+	}
 }
 
 
 // we may not be needing this function as we can create a an input .xml file to include all possible inputs
 // so, no agents will create another agent
-__FLAME_GPU_FUNC__ int create_crystals(xmachine_memory_crystal* agent, xmachine_message_crystal_list* internal_coords,RNG_rand48* rand48){
+__FLAME_GPU_FUNC__ int create_ranks(xmachine_memory_crystal* agent, xmachine_message_crystal_list* internal_coords,RNG_rand48* rand48){
 
 
 // put this in another file where you create agents
@@ -75,69 +82,65 @@ __FLAME_GPU_FUNC__ int create_crystals(xmachine_memory_crystal* agent, xmachine_
 	// cube root of the mean volume of the charge?
 
 	//output crystal internal coordinates
-    add_internal_coord(internal_coords, agent->rank, agent->l);
+
+	//generate a new rank for the iteration
+	agent->rank = rnd<CONTINUOUS>(rand48);
+
+	//output ranks and length
+    	add_internal_coord(internal_coords, agent->rank, agent->l);
 
 	return 0;
 }
 
 
-__FLAME_GPU_FUNC__ int check_aggregate(xmachine_memory_crystal* agent, xmachine_message_internal_coords_list* internal_coords_messages){
+__FLAME_GPU_FUNC__ int aggregate(xmachine_memory_crystal* agent, xmachine_message_internal_coords_list* internal_coords_messages){
 
- xmachine_message_internal_coords* current_message = get_first_internal_coords_message(internal_coords_messages);
 
-int lower_rank=0;
+	int lower_rank_count=0;	//count of agents that have a lower rank that the current agent
+	float closest_lower_rank = 0;
+	float closest_lower_length = 0;
+	int destroy = 0;
 
-    while (current_message)
-    {
+	//iterate through messages
+	xmachine_message_internal_coords* current_message = get_first_internal_coords_message(internal_coords_messages);
+    	while (current_message)
+    	{
 		//check the rank to be less than aggNo
-		if (current_message->rank <= agent->rank){
-			lower_rank++;
+		if (current_message->rank < agent->rank){
+			lower_rank_count++;
+			if (current_message->rank > closest_lower_rank){
+				closest_lower_rank = current_message->rank;
+				closest_lower_length = current_message->l;
+			}
+			
 		}
         
-        current_message = get_next_internal_coords_message(current_message, internal_coords_messages);
-    }
+        	current_message = get_next_internal_coords_message(current_message, internal_coords_messages);
+    	}
    
-   if (lower_rank < 2*aggNo)// or 2*aggNo
-   	agent->agg_flag=1;
-
-    return 0;
-}
-
-
-__FLAME_GPU_FUNC__ int aggregate(xmachine_memory_crystal* agent, xmachine_message_internal_coords_list* internal_coords_messages,RNG_rand48* rand48){
-
- xmachine_message_internal_coords* current_message = get_first_internal_coords_message(internal_coords_messages);
-
-
-int aggregated=0;
-
-if (agent->agg_flag = 0)
-	return 0;
-
-else{	
-    while (current_message)
-    {
-		//check the aggregation flag to be one, so that they can aggregate NOW
-		if (current_message->agg_flag == 1){
-
-			int l_agg = ((agent->l ^ 3)+(current_message->l^3))^(1/3); // adding the new length
-			float rand_rank=rnd<DISCRETE_2D>(rand48); 
-			//add_internal_coord(internal_coords, rand_rank, l_agg, 0); // first case study ignores growth
-
-//Note: can't kill two agents at the same time. Instead of creating a function to do that, we replace one with new parameters
-			current_message->aggregated=0;
-			current_message->rank=rand_rank;
-			current_message->l=l_agg;
-
-			aggregated=1;
+	//calculate if the agent has a low enough rank to aggregate
+	if (lower_rank_count < 2*aggNo){
+		//even numbered agents will be destroyed
+		if(lower_rank_count % 2 == 0){
+			destroy = 1;
 		}
-        
-        current_message = get_next_internal_coords_message(current_message, internal_coords_messages);
-    }
+		//odd numbered agents will be aggregated
+		else{
+			//apply the formular to find the aggregate length (equ 8)
+			agent->l = ((agent->l ^ 3)+(closest_lower_length ^ 3)) ^ (1/3); //todo: needs to use powf()
+		}
+
+
+
+	}
+
+	//calculate the bin
+	agent->bin = agent->l / BIN_WIDTH;
+
+    return destroy;
 }
- 
-     return aggregated;
-}
+
+
 
 
 #endif //_FLAMEGPU_FUNCTIONS
