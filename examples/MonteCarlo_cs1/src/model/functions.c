f8126e7a4ce9490a95dad9730af033c2866a42c7
Mozhgan K. Chimeh <mozhgan.kabiri@gmail.com>
2016-11-09 14:55:27 +0000

    MonteCarlo_CS1 fixed inpGen

--- examples/MonteCarlo_cs1/src/model/functions.c
+++ examples/MonteCarlo_cs1/src/model/functions.c
@@ -5,34 +5,45 @@
  * Contact: p.richmond@sheffield.ac.uk (http://www.paulrichmond.staff.shef.ac.uk)
  *          m.kabiri-chimeh@sheffield.ac.uk (http://www.mkchimeh.staff.shef.ac.uk)
  *
- * University of Sheffield retain all intellectual property and 
- * proprietary rights in and to this software and related documentation. 
- * Any use, reproduction, disclosure, or distribution of this software 
+ * University of Sheffield retain all intellectual property and
+ * proprietary rights in and to this software and related documentation.
+ * Any use, reproduction, disclosure, or distribution of this software
  * and related documentation without an express license agreement from
  * University of Sheffield is strictly prohibited.
  *
- * For terms of licence agreement please attached licence or view licence 
+ * For terms of licence agreement please attached licence or view licence
  * on www.flamegpu.com website.
- * 
+ *
  */
 
 #ifndef _FLAMEGPU_FUNCTIONS
 #define _FLAMEGPU_FUNCTIONS
 
 #include <header.h>
+#include <cuda.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
 
 #define I_agg 0.875f // high index aggregation
 #define aggNo 1
 
+#define L_MAX 2.0
+#define NL_MAX 0.4
+#define BIN_WIDTH 0.1
+#define BIN_COUNT L_MAX/BIN_WIDTH
+
 float *d_dt;
-
-__device__ float normcdff (float y); //Calculate the standard normal cumulative distribution function
-
+void gpuAssert(cudaError_t code, const char *file, int line, bool abort);
+/* Error check function for safe CUDA API calling */
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__, true); }
 
 
 __FLAME_GPU_INIT_FUNC__ void initConstants()
 {
-	float i_agg = 0.0.875f; // high index aggregation
+	float i_agg = 0.875f; // high index aggregation
 	set_I_agg(&i_agg);
 
 	int aggno = 1;
@@ -45,13 +56,15 @@ __FLAME_GPU_INIT_FUNC__ void initConstants()
 // The function calculates the time step needed for CASE STUDY 1 - NOT USING IT !
 __FLAME_GPU_STEP_FUNC__ void DELTA_T_func(){
 
-	beta0 = h_I_agg; // it's the aggregation number
+	float beta0 = *get_I_agg(); // it's the aggregation Index number
+  int agg_No = *get_aggNo();  // it's the aggregation number
+  float popnNo;
 
 	gpuErrchk(cudaMemcpyFromSymbol( &popnNo,d_xmachine_memory_crystal_count, sizeof(float))); // Nc-aggNo
 
-	float dt = ((2*h_aggNo)/(beta0*popnNo*(1-h_I_agg)*(popnNo-h_aggNo))); // equation 10 and 11 
+	float dt = ((2*agg_No)/(beta0*popnNo*(1-beta0)*(popnNo-agg_No))); // equation 10 and 11
 
-	cudamalloc((void**)&d_dt, sizeof(float));
+	gpuErrchk(cudaMalloc((void**)&d_dt, sizeof(float)));
 	gpuErrchk(cudaMemcpy(d_dt, &dt, sizeof(float), cudaMemcpyHostToDevice));
 
 
@@ -59,27 +72,47 @@ __FLAME_GPU_STEP_FUNC__ void DELTA_T_func(){
 }
 
 
-__FLAME_GPU_EXIT_FUNC__ void exitFunction(){
+__FLAME_GPU_EXIT_FUNC__ void hist_func(){
+
+  static long iter = 0;
+  iter++;
+
+  char * filename = "hist";
+
+  //char *integer_string;
+ // sprintf(integer_string, "%ld", iter);
+
+  //strcat (filename,integer_string);
+  //strcat (filename,".dat");
+
+ // printf("%s\n", filename);
+
+ stringstream sstm;
+
+ sstm << filename << iter;
+
+ string ans = sstm.str();
+ char * ans2 = (char *) ans.c_str();
+
 
 	printf("FLAME GPU Exit function");
+  FILE *hist_output = fopen(ans2, "a"); // write only - append
+
 	for (int i=0; i<BIN_COUNT; i++){
 		int count = count_crystal_default_bin_variable(i);
 		printf("bin index=%d, count = %d", i, count);
-		//output into same format as initial states
-	}
+    fprintf(hist_output,"%f %d\n", i*BIN_WIDTH, count);
+    //output into same format as initial states
+  }
+  fprintf(hist_output,"\n\n\n");
+  fclose(hist_output);
 }
 
 
 // we may not be needing this function as we can create a an input .xml file to include all possible inputs
 // so, no agents will create another agent
-__FLAME_GPU_FUNC__ int create_ranks(xmachine_memory_crystal* agent, xmachine_message_crystal_list* internal_coords,RNG_rand48* rand48){
+__FLAME_GPU_FUNC__ int create_ranks(xmachine_memory_crystal* agent, xmachine_message_internal_coord_list* internal_coord,RNG_rand48* rand48){
 
-
-// put this in another file where you create agents
-	//float growth = normcdff(rnd<DISCRETE_2D>(rand48));
-    //float length = normcdff(rnd<DISCRETE_2D>(rand48)); 
-	// required size distribution ?
-	// cube root of the mean volume of the charge?
 
 	//output crystal internal coordinates
 
@@ -87,13 +120,13 @@ __FLAME_GPU_FUNC__ int create_ranks(xmachine_memory_crystal* agent, xmachine_mes
 	agent->rank = rnd<CONTINUOUS>(rand48);
 
 	//output ranks and length
-    	add_internal_coord(internal_coords, agent->rank, agent->l);
+    	add_internal_coord_message(internal_coord, agent->rank, agent->l);
 
 	return 0;
 }
 
 
-__FLAME_GPU_FUNC__ int aggregate(xmachine_memory_crystal* agent, xmachine_message_internal_coords_list* internal_coords_messages){
+__FLAME_GPU_FUNC__ int aggregate(xmachine_memory_crystal* agent, xmachine_message_internal_coord_list* internal_coord_messages){
 
 
 	int lower_rank_count=0;	//count of agents that have a lower rank that the current agent
@@ -102,7 +135,7 @@ __FLAME_GPU_FUNC__ int aggregate(xmachine_memory_crystal* agent, xmachine_messag
 	int destroy = 0;
 
 	//iterate through messages
-	xmachine_message_internal_coords* current_message = get_first_internal_coords_message(internal_coords_messages);
+	xmachine_message_internal_coord* current_message = get_first_internal_coord_message(internal_coord_messages);
     	while (current_message)
     	{
 		//check the rank to be less than aggNo
@@ -112,12 +145,12 @@ __FLAME_GPU_FUNC__ int aggregate(xmachine_memory_crystal* agent, xmachine_messag
 				closest_lower_rank = current_message->rank;
 				closest_lower_length = current_message->l;
 			}
-			
+
 		}
-        
-        	current_message = get_next_internal_coords_message(current_message, internal_coords_messages);
+
+        	current_message = get_next_internal_coord_message(current_message, internal_coord_messages);
     	}
-   
+
 	//calculate if the agent has a low enough rank to aggregate
 	if (lower_rank_count < 2*aggNo){
 		//even numbered agents will be destroyed
@@ -127,7 +160,7 @@ __FLAME_GPU_FUNC__ int aggregate(xmachine_memory_crystal* agent, xmachine_messag
 		//odd numbered agents will be aggregated
 		else{
 			//apply the formular to find the aggregate length (equ 8)
-			agent->l = ((agent->l ^ 3)+(closest_lower_length ^ 3)) ^ (1/3); //todo: needs to use powf()
+			agent->l = powf((powf(agent->l, 3)+ powf(closest_lower_length, 3)), (1/3)); //todo: needs to use powf()
 		}
 
 
--- examples/MonteCarlo_cs2/src/model/functions.c
+++ examples/MonteCarlo_cs2/src/model/functions.c
@@ -5,15 +5,15 @@
  * Contact: p.richmond@sheffield.ac.uk (http://www.paulrichmond.staff.shef.ac.uk)
  *          m.kabiri-chimeh@sheffield.ac.uk (http://www.mkchimeh.staff.shef.ac.uk)
  *
- * University of Sheffield retain all intellectual property and 
- * proprietary rights in and to this software and related documentation. 
- * Any use, reproduction, disclosure, or distribution of this software 
+ * University of Sheffield retain all intellectual property and
+ * proprietary rights in and to this software and related documentation.
+ * Any use, reproduction, disclosure, or distribution of this software
  * and related documentation without an express license agreement from
  * University of Sheffield is strictly prohibited.
  *
- * For terms of licence agreement please attached licence or view licence 
+ * For terms of licence agreement please attached licence or view licence
  * on www.flamegpu.com website.
- * 
+ *
  */
 
 #ifndef _FLAMEGPU_FUNCTIONS
@@ -22,13 +22,18 @@
 #include <header.h>
 
 #define DELTA_T 0.0005f //time step
-#define B0 210^8f 
+#define B0 2*10^8f
 #define G0 0.0.00168f
 #define b 0.22615f
 #define y 1f
 #define m0 1 // real number of crystals per unit volume
 
-    int *nuclNo;
+#define L_MAX 2.0
+#define NL_MAX 0.4
+#define BIN_WIDTH 0.1
+#define BIN_COUNT L_MAX/BIN_WIDTH
+
+int *nuclNo;
 
 __FLAME_GPU_INIT_FUNC__ void initConstants()
 {
@@ -57,17 +62,27 @@ __FLAME_GPU_INIT_FUNC__ void initConstants()
 // The function calculates the nucleation number needed for CASE STUDY 2
 __FLAME_GPU_STEP_FUNC__ void nuclNo_func(){
 
-	int nc_no = h_B0*h_DELTA_T*10^7; // equation 13, nuclNo = (B0*DELTA_T*popnNo)/m0, m0/Nc is the order of 10^7 
-    
+	int nc_no = h_B0*h_DELTA_T*10^7; // equation 13, nuclNo = (B0*DELTA_T*popnNo)/m0, m0/Nc is the order of 10^7
+
 	cudamalloc((void**)&nuclNo, sizeof(int));
     gpuErrchk(cudaMemcpy(nuclNo, &nc_no, sizeof(int), cudaMemcpyHostToDevice));
 
 	printf("FLAME GPU Step function. nuclNo is %d\n", nc_no);
 }
 
-__FLAME_GPU_EXIT_FUNC__ void exitFunction(){
+__FLAME_GPU_EXIT_FUNC__ void hist_func(){
 
 	printf("FLAME GPU Exit function");
+	FILE *hist_output = fopen("hist.dat", "a"); // write only - append
+
+	for (int i=0; i<BIN_COUNT; i++){
+		int count = count_crystal_default_bin_variable(i);
+		printf("bin index=%d, count = %d", i, count);
+		fprintf(hist_output,"%f %d\n", i*BIN_WIDTH, count);
+		//output into same format as initial states
+	}
+		fprintf(hist_output,"\n\n\n");
+	fclose(hist_output);
 }
 
 // we may not be needing this function as we can create a an input .xml file to include all possible inputs
@@ -75,7 +90,7 @@ __FLAME_GPU_EXIT_FUNC__ void exitFunction(){
 __FLAME_GPU_FUNC__ int output_crystals(xmachine_memory_crystal* agent, xmachine_message_crystal_list* internal_coords){
 
 	//output crystal internal coordinates
-    add_internal_coord(internal_coords, agent->l);
+    add_internal_coord(internal_coords, agent->l, 0);
 
 	return 0;
 }
@@ -96,7 +111,7 @@ add_internal_coord(internal_coords, 0);
 
 
 __FLAME_GPU_FUNC__ int growth(xmachine_memory_crystal* agent){
- 
+
  int l_new = agent->l + (DELTA_T*G0*((1+y*agent->l)^b)); // equation 20, Lj = Lj + Gj*DELTA_T
 
  agent->l = l_new
