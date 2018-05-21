2dcbe0f734909e2e8d2db6fd8ff28fc2a9b44dc5
Peter Heywood <peethwd@gmail.com>
2018-05-21 16:15:51 +0100

    Rename <non-zero-int>.xml to 0.xml for examples

--- bin/x64/CirclesPartitioning_double.bat
+++ bin/x64/CirclesPartitioning_double.bat
@@ -1,1 +1,1 @@
-"Release_Console\CirclesPartitioning_double.exe" "..\..\examples\CirclesPartitioning_double\iterations\1024.xml" 1
+"Release_Console\CirclesPartitioning_double.exe" "..\..\examples\CirclesPartitioning_double\iterations\0.xml" 1
--- bin/x64/CirclesPartitioning_float.bat
+++ bin/x64/CirclesPartitioning_float.bat
@@ -1,1 +1,1 @@
-"Release_Console\CirclesPartitioning_float.exe" "..\..\examples\CirclesPartitioning_float\iterations\1024.xml" 1
+"Release_Console\CirclesPartitioning_float.exe" "..\..\examples\CirclesPartitioning_float\iterations\0.xml" 1
